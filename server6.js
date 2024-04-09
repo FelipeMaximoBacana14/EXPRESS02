f148218d42ed333d1dc897951b792af0ad2557bd
Professor marcelo <avalia.aob2023@gmail.com>
2024-04-09 16:34:10 -0300

    subir servidores finais

---
+++ server6.js
@@ -0,0 +1,25 @@
+//incorporar respostas a partir de outros arquivos
+const express = require('express');
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("Servidor na área bro")
+})
+
+const funcoes=require('./functions.js')
+
+app.get('/add/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b =parseInt(req.params.b);
+    const resultado =funcoes.add(a,b);
+    res.send(`A soma de ${a} e ${b} é igual a ${resultado}`);
+})
+
+app.get('/sub/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b =parseInt(req.params.b);
+    const resultado =funcoes.sub(a,b);
+    res.send(`A diferença de ${a} e ${b} é igual a ${resultado}`);
+})
+
+
